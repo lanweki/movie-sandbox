47bbe71ef7a5023ee594b88fcab5389206ca0a70
lanweki <futurelanweki@gmail.com>
2024-10-17 15:31:37 +0200

    Initial commit

---
+++ src/main/java/lt/lanweki/ainiuseva/MovieSandboxApplication.java
@@ -0,0 +1,13 @@
+package lt.lanweki.ainiuseva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieSandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieSandboxApplication.class, args);
+	}
+
+}
